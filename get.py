ca389c56a330e66786eaad3a4adeef21dc522331
RB <jugnme@126.com>
2022-07-10 13:45:26 +0800

    Add files via upload

---
+++ get.py
@@ -0,0 +1,58 @@
+import requests
+import copy
+
+test = BaseException()
+autofill_schema = lambda url_list: ['http:'+each if each.startswith('//') else each for each in url_list]
+
+def geturls(uri: str, headers: dict, autofill=False, proxy=False, http_proxy='', https_proxy=''):
+    try:
+        if proxy:
+            raw = requests.get(uri,headers=headers,proxies={'http': http_proxy, 'https': https_proxy}).text
+        else:
+            raw = requests.get(uri, headers=headers).text
+    except Exception:
+        return []
+    raw_list = list(raw)
+    urls = []
+
+    for i, chr in enumerate(raw_list):
+        if chr == '"':
+            raw_list[i] = "'"
+    lines = ''.join(raw_list).split('\n')
+    # print(lines)
+
+    keywords = ["'//", "'http://", "'https://"]
+    for keyword in keywords:
+        for line in lines:
+            while line.find(keyword) != -1 and line.find("'",len(keyword)) != -1:
+                urls.append(keyword[1:]+line[line.find(keyword)+len(keyword):line.find("'",line.find(keyword)+1)])
+                line = line[line.find("'",line.find(keyword)+1):]
+
+    # print(urls)
+    if autofill:
+        return autofill_schema(urls)
+    else:
+        return urls
+
+def geturls_recur(url_key:str, uri: str, **other_para_of_geturls):
+    urls = set()
+
+    def core(uri: str, **other_para_of_geturls):
+        nonlocal urls
+        current_urls = list(filter(lambda url: url_key in url, set(geturls(uri, **other_para_of_geturls)).difference(urls)))
+        print(f'current_urls = {current_urls}')
+        if current_urls:
+            urls = urls.union(current_urls)
+            for each in current_urls:
+                under = core(each, **other_para_of_geturls)
+                if under:   urls = urls.union(under)
+        else:
+            print(f'Layer returned: {urls}')
+            return urls
+
+    return core(uri, **other_para_of_geturls)
+
+if __name__ == '__main__':
+    result = list(geturls_recur('sina', 'https://news.sina.com.cn', headers={'User-Agent': 'curl/7.29.0'}, autofill=True, proxy=False))
+    with open('result.txt', 'w') as fil:
+        print(*result, file=fil, sep='\n')
